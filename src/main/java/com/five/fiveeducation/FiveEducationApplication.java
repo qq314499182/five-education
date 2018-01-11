74b3851e7642d728d2c92c376b667cc99e34e8f2
zhaoke <zhaokep@yonyou.com>
2018-01-11 10:35:53 +0800

    初始化项目,修改pom文件

--- src/main/java/com/five/fiveeducation/FiveEducationApplication.java
+++ src/main/java/com/five/fiveeducation/FiveEducationApplication.java
@@ -2,9 +2,16 @@ package com.five.fiveeducation;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class FiveEducationApplication {
+public class FiveEducationApplication extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(FiveEducationApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(FiveEducationApplication.class, args);

d83a8639da57e6d4250fe59c00d0c9e768789469
zhaoke <zhaokep@yonyou.com>
2018-01-11 10:23:41 +0800

    Initial commit

---
+++ src/main/java/com/five/fiveeducation/FiveEducationApplication.java
@@ -0,0 +1,12 @@
+package com.five.fiveeducation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FiveEducationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FiveEducationApplication.class, args);
+	}
+}
